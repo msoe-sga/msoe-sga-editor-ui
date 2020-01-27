fb40d0d0a32a4421742e6b1483a407c2bffce1eb
andy-wojciechowski <wojciechowskia@msoe.edu>
2020-01-26 19:36:40 -0600

    Adding node-sass

--- src/views/PageLayout.jsx
+++ src/views/PageLayout.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Header } from '../components/layout/Layout';
+import * as styles from './PageLayout.scss';
 const logo = require('../assets/logo.png');
 
 export default function PageLayout() {
     return (
         <>
             <Header>
-                <img alt="Logo" src={logo}></img>
+                <img alt="Logo" src={logo} className={styles.logo}></img>
                 <h1>MSOE Student Government Website Editor</h1>
             </Header>
         </>
