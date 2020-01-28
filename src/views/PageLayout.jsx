e3b1e104700bef5b1425bf5dcf8cad75af6ea0aa
andy-wojciechowski <wojciechowskia@msoe.edu>
2020-01-27 18:14:29 -0600

    Fixed the alignment of the sidebar items

--- src/views/PageLayout.jsx
+++ src/views/PageLayout.jsx
@@ -14,6 +14,7 @@ export default function PageLayout() {
             </Header>
             <Sidebar>
                 <SidebarItem text="Editors"></SidebarItem>
+                <SidebarItem text="Temp"></SidebarItem>
             </Sidebar>
         </>
     );
