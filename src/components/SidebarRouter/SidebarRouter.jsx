31d38c8e78b493073563bc046422bb889b1e2d3b
andy-wojciechowski <wojciechowskia@msoe.edu>
2020-02-16 19:32:13 -0600

    Adding a editor for the Sidebar

---
+++ src/components/SidebarRouter/SidebarRouter.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Switch, Route, Link, useRouteMatch } from 'react-router-dom';
+import Editors from '../editors';
+
+const paths = {
+    editors: {
+        path: 'editors',
+        displayText: 'Editors',
+        onClick: (setLocalState) => {
+            setLocalState(null); // Do this to clear view when switching
+            // TODO: Call the API to get the list of editors
+        }
+    }
+};
+
+export default function SidebarRouter() {
+    const { url, path } = useRouteMatch();
+    const [switchContent, setSwitchContent] = React.useState(null);
+
+    return (
+        <div>
+            <div>
+                {Object.keys(paths).map(path => {
+                    const toPath = `${url}/${paths[path].path}`;
+                    return (
+                        <Link
+                            to={toPath}
+                            key={path}
+                            onClick={
+                                () => paths[path].onClick(setSwitchContent)
+                            }
+                        >
+                            <span>{paths[path].displayText}</span>
+                        </Link>
+                    );
+                })}
+            </div>
+            <Switch>
+                <Route exact={true} path={`${path}/${paths.editors.path}`}>
+                    <Editors editors={switchContent} />
+                </Route>
+            </Switch>
+        </div>
+    );
+}
---
+++ src/components/editors/Editors.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Editors({ editors }) {
+    return (
+        <div>Hello World!</div>
+    );
+}
