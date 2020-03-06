56c29fb30781060b4995574f953630662a0c6c26
andy-wojciechowski <wojciechowskia@msoe.edu>
2020-03-05 19:38:32 -0600

    Adding react-table and adding displaying editors in a table

--- src/components/editors/Editors.jsx
+++ src/components/editors/Editors.jsx
@@ -2,11 +2,40 @@ import React from 'react';
 import { getAllEditors } from '../../api/editors';
 import { useSelector, useDispatch } from 'react-redux';
 import { setEditors } from '../../api/state/actions';
+import {
+    useTable,
+    useGroupBy,
+    useFilters,
+    useSortBy,
+    useExpanded,
+    usePagination
+} from 'react-table';
 
 export default function Editors() {
+    const tableColumns = [
+        {
+            Header: 'Editors',
+            columns: [
+                {
+                    Header: "Id",
+                    accessor: "id"
+                },
+                {
+                    Header: "Name",
+                    accessor: "name"
+                },
+                {
+                    Header: "Email",
+                    accessor: "email"
+                }
+            ]
+        }
+    ];
+
     const editors = useSelector(state => state.editors);
     const [isLoading, setIsLoading] = React.useState(true);
     const dispatch = useDispatch();
+    let tableEditors = [];
 
     getAllEditors(json => {
         if (isLoading) {
@@ -15,10 +44,54 @@ export default function Editors() {
         }
     });
 
+    if (editors !== null) {
+        tableEditors = editors.map(editor => {
+            return {
+                id: editor.id,
+                name: editor.fields.Name,
+                email: editor.fields.Email
+            }
+        });
+    }
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+      } = useTable({
+        columns: tableColumns,
+        data: tableEditors,
+    });
+
     return (
         <div>
             {isLoading && <div>Loading...</div>}
-            {!isLoading && <div>We are loaded!</div>}
+            {!isLoading && 
+            <table {...getTableProps()}>
+            <thead>
+              {headerGroups.map(headerGroup => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map(column => (
+                    <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                  ))}
+                </tr>
+              ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+              {rows.map((row, i) => {
+                prepareRow(row)
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map(cell => {
+                      return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                    })}
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>}
         </div>
     );
 }
