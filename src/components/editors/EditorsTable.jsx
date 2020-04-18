b6b36b84f4e3c3a1f50fe123ae23a98d4835c12c
andy-wojciechowski <wojciechowskia@msoe.edu>
2020-04-18 13:10:21 -0500

    Making some more progress on the editors component

--- src/components/editors/EditorsTable.jsx
+++ src/components/editors/EditorsTable.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getAllEditors, createEditor } from '../../api/editors';
+import { getAllEditors, createEditor, updateEditor } from '../../api/editors';
 import { useSelector, useDispatch } from 'react-redux';
 import { setEditors } from '../../api/state/actions';
 import { useTable, usePagination } from 'react-table';
@@ -20,14 +20,38 @@ export default function EditorsTable() {
                     accessor: "email"
                 }
             ]
+        },
+        {
+          Header: () => null,
+          id: 'edit',
+          Cell: ({ row }) => (
+            <button onClick={() => editEditorRowOnClick(row)}>Edit</button>
+          )
+        },
+        {
+          Header: () => null,
+          id: 'delete',
+          Cell: ({ row }) => (
+            <button onClick={() => deleteEditorRowOnClick(row)}>Delete</button>
+          )
         }
     ], []);
 
     const editors = useSelector(state => state.editors);
     const [isLoading, setIsLoading] = React.useState(true);
+
     const [isCreateEditorModalOpen, setIsCreateEditorModalOpen] = React.useState(false);
     const [createEditorModalName, setCreateEditorModalName] = React.useState(null);
     const [createEditorModalEmail, setCreateEditorModalEmail] = React.useState(null);
+
+    const [isEditEditorModalOpen, setIsEditEditorModalOpen] = React.useState(false);
+    const [editEditorModalId, setEditEditorModalId] = React.useState(null);
+    const [editEditorModalName, setEditEditorModalName] = React.useState(null);
+    const [editEditorModalEmail, setEditEditorModalEmail] = React.useState(null);
+
+    const [isDeleteEditorConfirmationModalOpen, setIsDeleteEditorConfirmationModalOpen] = React.useState(false);
+    const [deleteEditorConfirmationModalId, setDeleteEditorConfirmationModalId] = React.useState(null);
+    const [deleteEditorConfirmationModalName, setDeleteEditorConfirmationModalName] = React.useState(null);
 
     const dispatch = useDispatch();
 
@@ -46,30 +70,45 @@ export default function EditorsTable() {
       }
     }), [editors]);
 
+    function editEditorRowOnClick(row) {
+      setEditEditorModalId(row.original.id);
+      setEditEditorModalName(row.original.name);
+      setEditEditorModalEmail(row.original.email);
+      setIsEditEditorModalOpen(true);
+    }
+
+    function deleteEditorRowOnClick(row) {
+      setDeleteEditorConfirmationModalId(row.original.id);
+      setDeleteEditorConfirmationModalName(row.original.name);
+      setIsEditEditorModalOpen(true);
+    }
+
     function openCreateEditorModal() {
       setCreateEditorModalName(null);
       setCreateEditorModalEmail(null);
       setIsCreateEditorModalOpen(true);
     }
 
-    function closeCreateEditorModal() {
-      setIsCreateEditorModalOpen(false);
-    }
-
     function onCreateEditorFormSubmit() {
-      closeCreateEditorModal();
+      setIsCreateEditorModalOpen(false);
       setIsLoading(true);
       createEditor(createEditorModalName, createEditorModalEmail, json => {
         setIsLoading(false);
       });
     }
 
-    function onCreateEditorModalNameChange(event) {
-      setCreateEditorModalName(event.target.value);
+    function onEditEditorFormSubmit() {
+      setIsLoading(true);
+      updateEditor(editEditorModalId, editEditorModalName, editEditorModalEmail, () => {
+        setIsLoading(false);
+      });
     }
 
-    function onCreateEditorModalEmailChange(event) {
-      setCreateEditorModalEmail(event.target.value);
+    function deleteEditor() {
+      setIsLoading(true);
+      deleteEditor(deleteEditorConfirmationModalId, () => {
+        setIsLoading(false);
+      });
     }
 
     const {
@@ -144,20 +183,44 @@ export default function EditorsTable() {
                 <button onClick={openCreateEditorModal} className={styles.createEditorButton}>Create Editor</button>
                 <Modal
                   isOpen={isCreateEditorModalOpen}
-                  onRequestClose={closeCreateEditorModal}
+                  onRequestClose={() => setIsCreateEditorModalOpen(false)}
                 >
                   <h2>Create Editor</h2>
                   <form onSubmit={onCreateEditorFormSubmit}>
                     <label>
                       Name:
-                      <input type="text" value={createEditorModalName} onChange={onCreateEditorModalNameChange} />
+                      <input type="text" value={createEditorModalName} onChange={(event) => setCreateEditorModalName(event.target.value)} />
                     </label>
                     <label>
                       Email:
-                      <input type="text" value={createEditorModalEmail} onChange={onCreateEditorModalEmailChange} />
+                      <input type="text" value={createEditorModalEmail} onChange={(event) => setCreateEditorModalEmail(event.target.value)} />
                     </label>
                     <button type="submit">Save</button>
                   </form>
+                </Modal>
+                <Modal
+                  isOpen={isEditEditorModalOpen}
+                  onRequestClose={() => setIsEditEditorModalOpen(false)}
+                >
+                  <h2>Edit Editor</h2>
+                  <form onSubmit={onEditEditorFormSubmit}>
+                    <label>
+                      Name:
+                      <input type="text" value={editEditorModalName} onChange={(event) => setEditEditorModalName(event.target.value)} />
+                    </label>
+                    <label>
+                      Email:
+                      <input type="text" value={editEditorModalEmail} onChange={(event) => setEditEditorModalEmail(event.target.value)} />
+                    </label>
+                    <button type="submit">Save</button>
+                  </form>
+                </Modal>
+                <Modal
+                  isOpen={isDeleteEditorConfirmationModalOpen}
+                  onRequestClose={() => setIsDeleteEditorConfirmationModalOpen(false)}
+                >
+                  <div>Are you sure you want to delete the editor {deleteEditorConfirmationModalName}?</div>
+                  <button> onClick={deleteEditor}Yes</button>
                 </Modal>
               </div>}
         </div>
