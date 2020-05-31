ef552c398b9d8966159f02758d28e32a457b0ddb
andy-wojciechowski <wojciechowskia@msoe.edu>
2020-05-31 09:43:59 -0500

    Starting on Google Auth

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import EditorsTable from './components/editors/EditorsTable';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import allReducers from './api/state/reducers';
+import GoogleLogin from 'react-google-login';
 
 const paths = {
     root: '/'
@@ -12,9 +13,14 @@ const paths = {
 
 const store = createStore(allReducers);
 
+function onLoginSuccess(response) {
+    console.log(response);
+}
+
 export default function App() {
     return (
         <Provider store={store}>
+            <GoogleLogin clientId="112125019226-r2f9eiejmqv1o6077ea7vtbanie22gfb.apps.googleusercontent.com" onSuccess={onLoginSuccess} isSignedIn={true} />
             <BrowserRouter>
                 <Switch>
                     <Route exact={true} path={paths.root}>
