c18dfeac865ad548000ec8d8cead025d5837c6c7
paulickj <paulickj@msoe.edu>
2020-10-14 23:07:49 +0000

    Adds posts.js with method calls

---
+++ src/api/posts.js
@@ -0,0 +1,55 @@
+import { getValidCallback, getHttpHeaders, getApiUrl } from './helpers';
+
+const url = getApiUrl();
+
+export function getAllPosts(googleToken, callback) {
+	const requestOptions = {
+		method: 'GET',
+		headers: getHttpHeaders(googleToken)
+	};
+
+	const validCallback = getValidCallback(callback);
+
+	fetch(`${url}/posts`, requestOptions)
+		.then(res => res.json())
+		.then(json => validCallback(json));
+}
+
+export function createPost(text, author, title, googleToken, callback) {
+	const requestOptions = {
+		method: 'POST',
+		headers: getHttpHeaders(googleToken),
+		body: JSON.stringify({
+			'text': text,
+			'author': author,
+			'title': title
+		})
+	};
+
+	const validCallback = getValidCallback(callback);
+
+        fetch(`${url}/posts`, requestOptions)
+                .then(res => res.json())
+                .then(json => validCallback(json));
+}
+
+export function editPost(text, author, title, path, ref, googleToken, callback) {
+	 const requestOptions = {
+                method: 'POST',
+                headers: getHttpHeaders(googleToken),
+                body: JSON.stringify({
+                        'text': text,
+                        'author': author,
+                        'title': title,
+			'path': path,
+			'rev': ref
+                })
+        };
+
+        const validCallback = getValidCallback(callback);
+
+        fetch(`${url}/posts`, requestOptions)
+                .then(res => res.json())
+                .then(json => validCallback(json));
+}
+
