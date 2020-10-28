ae416586489e401c145e0ba7b1ea0f1445a6c111
paulickj <paulickj@msoe.edu>
2020-10-28 23:03:13 +0000

    Started hooking up Post List component from api

--- src/elements/postListing/PostListPage.jsx
+++ src/elements/postListing/PostListPage.jsx
@@ -2,41 +2,72 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import postData from './testPostData.json';
 import { Link } from 'react-router-dom';
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
+import { getAllPosts } from '../../api/posts';
+import Alert from 'react-bootstrap/Alert';
 
 export default function PostListPage() {
 
-  	 const history = useHistory();
-	   const goToNewPost = () =>{ 
-	    let path = `newPath`; 
-	    history.push('/posts/new');
-	  }
+    const history = useHistory();
+    const goToNewPost = () =>{
+        let path = `newPath`; 
+        history.push('/posts/new');
+    }
+
+    const [isLoading, setIsLoading] = React.useState(true);
+    const [errorMessage, setErrorMessage] = Reackt.useState(null);
+
+    const [posts, setPosts] = React.useState(null);
+
+    React.useEffect(() => {
+        if (isLoading) {
+            getAllPosts(authToken, json => {
+                if (json.isAuthorized === false) {
+                    dispatch(setAuthError(json.error));
+                    history.push('/');
+                } else if (json.error) {
+                    setErrorMessage(json.error);
+                } else {
+                    posts = setPosts(json.existingPosts);
+                }
+                setIsLoading(false);
+            });
+        }
+    });
 
 
-	return <div className="pt-2">
-	<h2 className="pb-2" >Posts</h2>
-	<Button variant="primary" onClick={goToNewPost}>+ New </Button>
-	<hr/>
+    return(
+        <div>
+            {isLoading && <div>Loading...</div>}
+	    {!isLoading &&
+                <div>
+                    {errorMessage && (
+                        <Alert variant='danger'>{error}</Alert>
+                    )}
+                    <div className="pt-2">
+                        <h2 className="pb-2" >Posts</h2>
+                        <Button variant="primary" onClick={goToNewPost}>+ New </Button>
+                        <hr/>
 
-	{Object.keys(postData.posts).map(post => {
-        
-        return (
-            <PostPreview key={postData.posts[post].title}
-            post={postData.posts[post]}/>
+                        {Object.keys(postData.posts).map(post => {
 
-        );
-    })}
+                            return (
+                                <PostPreview key={postData.posts[post].title}
+                                post={postData.posts[post]}/>
+                            );
+                        })}
 
-	</div>
+	            </div>
+                </div>}
 
+        </div>
+    );
 }
 
 
 
 const PostPreview = (props) => {
 
-  const dateString = new Date(props.post.date).toDateString();
-  console.log(dateString);
   return (
 
     <div className="mb-5">
@@ -44,7 +75,6 @@ const PostPreview = (props) => {
 
    
     	<div>Author: {props.post.author}</div>
-    	<div>Date: {dateString}</div>
 
     </div>
   )
